e903488fbb4dbcb39b5ea592e3eba0d6a5b00e19
Nicolas Luna <nicolasluna.ca@gmail.com>
2024-10-18 23:46:13 -0300

    300

--- appUSERS/urls.py
+++ appUSERS/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('register/', views.CreateUsuarioView.as_view()),
     path('login/', views.CreateTokenView.as_view()),
     path('logout/', views.LogoutView.as_view()),
-    path('delete/', views.DeleteUsuarioView.as_view(), name='delete_usuario'),
-    path('update/', views.RetrieveUpdateUsuarioView.as_view(), name='retrieve_update_usuario')
+    path('profile/update/', views.RetrieveUpdateUsuarioView.as_view(), name='update_profile'),
+    path('profile/delete/', views.DeleteUsuarioView.as_view(), name='delete_profile'),
 ]
 
  
