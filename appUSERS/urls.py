75c58e49181414c40f637eb2d44d4bc3baacdb0f
Nicolas Luna <nicolasluna.ca@gmail.com>
2025-05-07 20:04:15 -0300

    Revert "90"
    This reverts commit 396d30a4fb8df4dac23033fe17ef2edf3e90c8ea.

--- appUSERS/urls.py
+++ appUSERS/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from appUSERS import views
-from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', views.CreateUsuarioView.as_view()),
@@ -8,6 +7,6 @@ urlpatterns = [
     path('logout/', views.LogoutView.as_view()),
     path('update/', views.UpdateProfileView.as_view(), name='update-profile'),
     path('delete/', views.DeleteProfileView.as_view(), name='delete-profile'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
+ 
