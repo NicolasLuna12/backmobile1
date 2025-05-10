dc0bbe4ca55af484815beb64ddd9833f2b079344
Nicolas Luna <nicolasluna.ca@gmail.com>
2025-05-09 22:46:39 -0300

    fix serializer

--- appUSERS/serializers.py
+++ appUSERS/serializers.py
@@ -2,10 +2,9 @@ from django.contrib.auth import get_user_model,authenticate
 from rest_framework import serializers
 
 class UsuarioSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = get_user_model()
-        fields = ['email','password','nombre','apellido','telefono']
+        fields = ['email','password','nombre','apellido','telefono','imagen_perfil']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self,validate_data):
