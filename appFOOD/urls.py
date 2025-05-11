e3fad495484c2d5acac955cd19955f7a2d74dc08
Nicolas Luna <nicolasluna.ca@gmail.com>
2025-05-11 18:27:50 -0300

    Revert "payu"
    This reverts commit 3b4c9ed059159a003e820b3732343c7d0af36c7c.

--- appFOOD/urls.py
+++ appFOOD/urls.py
@@ -2,11 +2,6 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from .views import ProductoViewSet
-from .payu_views import (
-    PayUConfigView,
-    PayUProcessPaymentView,
-    PayUNotificationView
-)
 
 router = DefaultRouter()
 router.register('', ProductoViewSet )
@@ -14,11 +9,7 @@ router.register('', ProductoViewSet )
 app_name = 'producto'
 
 
+
 urlpatterns = [
     path('', include(router.urls)),
-    
-    # PayU endpoints
-    path('payu/config/', PayUConfigView.as_view(), name='payu-config'),
-    path('payu/process-payment/', PayUProcessPaymentView.as_view(), name='payu-process-payment'),
-    path('payu/notification/', PayUNotificationView.as_view(), name='payu-notification'),
 ]
