00a5be6618c08a078c7990e3d326ce6ecfac6e19
Nicolas Luna <nicolasluna.ca@gmail.com>
2025-05-11 03:37:56 -0300

    Revert "cambio mp"
    This reverts commit 401fc4ed1d4ceab7b7a097da29a6ef06fcb37448.

--- appFOOD/urls.py
+++ appFOOD/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import ProductoViewSet
-from .mercadopago_views import MercadoPagoPreferenceView, MercadoPagoPaymentView, MercadoPagoProcessPaymentView
+from .views import ProductoViewSet, MercadoPagoPreferenceView
 
 router = DefaultRouter()
 router.register('', ProductoViewSet )
@@ -13,6 +12,4 @@ app_name = 'producto'
 urlpatterns = [
     path('', include(router.urls)),
     path('mercadopago/preference/', MercadoPagoPreferenceView.as_view(), name='mercadopago-preference'),
-    path('mercadopago/payment/', MercadoPagoPaymentView.as_view(), name='mercadopago-payment'),
-    path('mercadopago/process-payment/', MercadoPagoProcessPaymentView.as_view(), name='mercadopago-process-payment'),
 ]
