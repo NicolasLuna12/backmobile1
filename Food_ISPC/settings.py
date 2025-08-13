11947f3a204e457821f245e8342707c39a8c9e88
Nicolas Luna <nicolasluna.ca@gmail.com>
2025-08-13 00:06:50 -0300

    Update settings.py

--- Food_ISPC/settings.py
+++ Food_ISPC/settings.py
@@ -139,11 +139,11 @@ WSGI_APPLICATION = 'Food_ISPC.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'railway'),
-        'USER': os.getenv('DB_USER', 'root'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
         'OPTIONS': {
             'sql_mode': 'traditional',
         },
