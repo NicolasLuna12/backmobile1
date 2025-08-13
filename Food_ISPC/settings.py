80ebece14816bdbf2386927809a93c6df5c91a0d
Nicolas Luna <nicolasluna.ca@gmail.com>
2025-08-12 23:44:22 -0300

    Disable DEBUG mode for production security

--- Food_ISPC/settings.py
+++ Food_ISPC/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lq50bruj9vl$-l1#drn=*iof2z%f%nl!j05(ce#*s$e2xhixc='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['backmobile1.onrender.com', 'localhost', '127.0.0.1']
 
